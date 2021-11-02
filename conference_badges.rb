e88802a0eed00780183952c19c4a47c2b7aca31f
Mtuozzo86 <mtuozzo86@gmail.com>
2021-11-02 08:06:17 -0400

    commit tree 047378e6a2bfdb4646ce4d04ff648cf616a4e57e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker (speaker)
+    "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(speakers)
+    speakers.map do |speaker|
+      badge_maker(speaker)
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(speakers)
+   batch_badge_creator(speakers).each do |badge|
+    puts badge
+    end
+   assign_rooms(speakers).each do |assignment|
+    puts assignment
+   end
+end
